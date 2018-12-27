4e9b07224892c41a1a68645c11a4d162e9ff03a4
Lusoline <angelique.mandy@gmail.com>
2018-12-27 14:14:01 +0100

    MAJ Vues
    Rajout d'une vue formulaire dossier.

--- Projet_AEA_UIL/App_Start/RouteConfig.cs
+++ Projet_AEA_UIL/App_Start/RouteConfig.cs
@@ -14,6 +14,18 @@ namespace Projet_AEA_UIL
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+         name: "Accueil",
+         url: "{controller}/{action}/{id}",
+         defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+      );
+
+            routes.MapRoute(
+               name: "ActionClient",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Client", action = "CreationDossier", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
